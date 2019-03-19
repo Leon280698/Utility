ff3dbb335d5ae284a27f020986e6693eaed41988
Leon Buckel <leon@buckel-baehr.de>
2019-03-19 21:00:56 +0100

    Get rid of warning

--- mathUtil.h
+++ mathUtil.h
@@ -24,7 +24,7 @@ namespace util::math{
 	// Aligns an integer to the next multiple of the specified alignment
 	template<typename T1, typename T2>
 	constexpr auto align(T1 value, T2 alignment)->decltype(value + alignment){
-		return (value + alignment - 1) & ~(alignment - 1);
+		return (value + alignment - 1) & ~static_cast<decltype(value + alignment)>(alignment - 1);
 	}
 
 	// Vector types
